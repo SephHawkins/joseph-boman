49d607cb06752f9fb84f136fc81cc4a7facc3c88
Joseph Boman <seph.hawkins@gmail.com>
2017-05-01 14:22:21 -0700

    Slideshow functionality

--- main.js
+++ main.js
@@ -163,7 +163,7 @@ class ImageSlideshow extends React.Component {
         return (
             <div>
                 <div className="circle-img">
-                    {images.map((image, index) => <img key={image.link} src={image.link} alt={image.alt} className={(index === this.state.activeImage) ? 'active' : ''} />)}
+                    {images.map((image, index) => <img key={image.link} src={image.link} alt={image.alt} className={(index === this.state.activeImage) ? 'active' : 'hidden'} />)}
                 </div>
                 <div className="img-selector">
                     {images.map((image, index) => <Dot key={image.link + '-dot'} number={index} active={(index === this.state.activeImage) ? true : false} handleClick={this.switchImage} />)}
@@ -189,7 +189,7 @@ class Dot extends React.Component {
 
     render() {
         return (
-            <span className={'dot' + ((this.state.active) ? ' active' : '')}></span>
+            <span className={'dot' + ((this.state.active) ? ' active' : '')} onClick={this.handleClick}></span>
         );
     }
 }
