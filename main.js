0ea15c995dda09dc109ab4a82cb85813b7c4f645
Joseph Boman <seph.hawkins@gmail.com>
2017-05-01 16:14:40 -0700

    Weird bug

--- main.js
+++ main.js
@@ -104,7 +104,7 @@ class App extends React.Component {
                     <FrontPage projects={projectsJSON} handleClick={this.handleNavigation} />
                 </div>
                 <div className='right-buffer'>
-                    <a className='arrow' onClick={history.back()}>➣</a>
+                    <a className='arrow'>➣</a>
                     <ImageSlideshow images={images} />
                     <h1>MonoVirus</h1>
                     <h2>Action RPG Dungeon Crawler Game</h2>
