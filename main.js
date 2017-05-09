d0d86cb2ed6bfaf154334d1f02b90de8f9cfe706
Joseph Boman <seph.hawkins@gmail.com>
2017-05-09 12:04:58 -0700

    Mobile Menu tweaks

--- main.js
+++ main.js
@@ -285,6 +285,10 @@ class App extends React.Component {
         });
     }
 
+    showMobileMenu() {
+        $('.mobile-menu').css({'top': '60px'});
+    }
+
     render() {
         return (
             <div>
@@ -294,7 +298,7 @@ class App extends React.Component {
                             <img src="http://josephboman.com/wp-content/themes/sephhawkins/images/logo-min.png" />
                             <p><span>oseph</span> oman</p>
                         </a>
-                        <svg height="60" width="80" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
+                        <svg onClick={this.showMobileMenu} height="60" width="80" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
                             <path d="M4,10h24c1.104,0,2-0.896,2-2s-0.896-2-2-2H4C2.896,6,2,6.896,2,8S2.896,10,4,10z M28,14H4c-1.104,0-2,0.896-2,2  s0.896,2,2,2h24c1.104,0,2-0.896,2-2S29.104,14,28,14z M28,22H4c-1.104,0-2,0.896-2,2s0.896,2,2,2h24c1.104,0,2-0.896,2-2  S29.104,22,28,22z"></path>
                         </svg>
                     </nav>
@@ -344,7 +348,7 @@ class MobileMenu extends React.Component {
     }
 
     handleClick(e){
-
+        e.preventDefault();
     }
 
     render(){
